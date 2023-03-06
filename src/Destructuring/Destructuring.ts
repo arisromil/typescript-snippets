04ec28c04cff093a6719390b1deb106710ed03b2
Aris Tan <arisromil@yahoo.com>
2023-03-06 15:08:46 -0700

    initial commit, starting with destructuring

---
+++ src/Destructuring/Destructuring.ts
@@ -0,0 +1,9 @@
+
+// Destructuring objects
+const person = { name: 'John', age: 30 };
+const {name, age } = person;
+
+// Destructuring Arrays
+const numbers = [1, 2,3];
+const [first, second, third] = numbers;
+
